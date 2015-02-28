f1892b18463d81374cf542ba8d2d0b80fc81701c
Vladimir Voevodsky <vladimir@ias.edu>
2015-02-27 19:43:46 -0500

    Started working on the construction of a morphism from an iter_sec of the lB0-system constructed from an lC-system

--- lBsystems_to_precategories.v
+++ lBsystems_to_precategories.v
@@ -7,7 +7,9 @@ Unset Automatic Introduction.
 Require Export lBsystems.lBsystems_T_fun_Tj_Ttj.
 Require Export lBsystems.lBsystems_S_fun.
 
-Require Export lBsystems.lB0systems . 
+Require Export lBsystems.lB0systems .
+
+Require Export lCsystems.lC_to_lB0.
 
 
 (** *** The first construction of the types Mor_from_B *)
@@ -19,8 +21,36 @@ in the associated lC-system. We do it for non-unital prelBsystems but the only s
 is actually required is an lBsystem_carrier and an operation of type S_ops_type satisfying
 axiom ax0. *)
 
-Definition iterated_sections { BB : prelBsystem_non_unital }
-           ( m : nat ) (Z : BB ) ( le : m <= ll Z ) : UU .
+Lemma iter_sec_inn { BB : prelBsystem_non_unital }
+      { m : nat } { Z : BB } ( gt0 : m > 0 ) ( lesm : S m <= ll Z )
+      ( s : Tilde_dd ( ftn m Z ) ) : S_dom s Z .
+Proof.
+  intros.
+  unfold S_dom .
+  set ( eq := pr2 s : dd s = ftn m Z ) . simpl in eq .
+  rewrite eq .
+  
+  apply ( isabove_X_ftnX gt0 ) .
+  apply ( natgehgthtrans _ _ _ lesm ( natgthsn0 _ ) ) .
+
+Defined.
+
+Lemma iter_sec_le { BB : prelBsystem_non_unital }
+      { m : nat } { Z : BB } ( gt0 : m > 0 ) ( lesm : S m <= ll Z )
+      ( s : Tilde_dd ( ftn m Z ) ) : m <= ll ( S_op s Z ( iter_sec_inn gt0 lesm s ) ) . 
+Proof.
+  intros.
+  rewrite S_ax0 .
+  assert ( leint := natgehandminusr _ _ 1 lesm ) . 
+  simpl in leint . 
+  rewrite natminuseqn in leint .
+  exact leint . 
+
+Defined.
+
+
+Definition iter_sec { BB : prelBsystem_non_unital }
+           ( m : nat ) ( Z : BB ) ( le : m <= ll Z ) : UU .
 Proof.
   intros until m . set ( S := @S_op BB ) . set ( sax0 := @S_ax0 BB ) . 
   induction m as [ | m IHm ] .
@@ -28,27 +58,20 @@ Proof.
 
   intros Z le .
   destruct ( natgehchoice _ _ ( natgehn0 m ) ) as [ gt0 | eq0 ] . 
-  assert ( inn : forall s : Tilde_dd ( ftn m Z ), S_dom s Z ) .
-  intro s .
-  unfold S_dom .
-  set ( eq := pr2 s : dd s = ftn m Z ) . simpl in eq .
-  rewrite eq .  
-  apply ( isabove_X_ftnX gt0 ) .
-  apply ( natgehgthtrans _ _ _ le ( natgthsn0 _ ) ) . 
-
-  assert ( le' : forall s : Tilde_dd ( ftn m Z ), m <= ll ( S s Z ( inn s ) ) ) . 
-  intro s . 
-  rewrite sax0 . 
-  assert ( leint := natgehandminusr _ _ 1 le ) . 
-  simpl in leint . 
-  rewrite natminuseqn in leint .
-  exact leint . 
-
+  set ( inn := iter_sec_inn gt0 le ) .
+  set ( le' := iter_sec_le gt0 le ) . 
   exact ( total2 ( fun s : Tilde_dd ( ftn m Z ) => IHm ( S s Z ( inn s ) ) (le' s) ) ) .
 
   exact ( Tilde_dd Z ) . 
 
 Defined.
+
+
+(* Definition iter_sec_m_eq_sn { BB : prelBsystem_non_unital }
+           { m n : nat } ( eq : m = S n )
+           ( Z : BB ) ( lem : m <= ll Z ) ( lesn : S n <= ll Z ) :
+  iter_sec m Z lem =
+  total2 ( fun s : Tilde_dd ( ftn m Z ) => iter_sec n ( S s Z ( inn s ) *)
 
 
 (** We now define morphisms X --> Y as iterated sections of the projection Tprod X Y --> X *)
@@ -57,7 +80,7 @@ Defined.
 Definition Mor_from_B { BB : lB0system_non_unital } ( X Y : BB ) : UU .
 Proof.
   intros.
-  refine ( iterated_sections ( ll Y ) ( Tprod X Y ) _ ) .
+  refine ( iter_sec ( ll Y ) ( Tprod X Y ) _ ) .
   rewrite ll_Tprod . 
   apply natlehmplusnm . 
 
@@ -138,6 +161,56 @@ Proof .
   exact fun12 . 
 
 Defined.
+
+
+
+
+(** We now start proving the comparison theorem showing that for lB0-systems of the form
+lB0_from_C CC our construction Mor_from_B recovers the morphisms in CC. *)
+
+
+
+(** A construction of a function from iterated_sections to morphisms *)
+
+Definition iter_sec_to_mor { CC : lCsystem } ( m : nat ) ( Z : CC ) ( le : m <= ll Z )
+           ( itrs : @iter_sec ( lB0_from_C CC ) m Z le ) : ftn m Z --> Z .
+Proof.
+  intros until m . 
+  induction m . 
+  intros . 
+  apply ( identity Z ) .
+
+  intros . 
+  simpl in itrs . 
+  destruct (natgehchoice m 0 (natgehn0 m)) as [ gt0 | eq0 ] .
+
+  set ( inn := (@iter_sec_inn ( lB0_from_C CC ) _ _ gt0 le ( pr1 itrs ))).
+  set ( le' := (@iter_sec_le ( lB0_from_C CC ) _ _ gt0 le ( pr1 itrs))).
+  assert ( s1 := IHm (@S_op ( lB0_from_C CC ) ( pr1 itrs ) Z inn) le' ( pr2 itrs ) ) . 
+
+  assert ( qn_from_S : @S_op ( lB0_from_C CC ) (pr1 itrs) Z inn --> Z ) .  
+  unfold S_op.
+  simpl .
+  unfold S_from_C .
+  exact ( qn (mor_to_pr2
+                (dd_from_C (Tilde_dd_pr1 (pr1 itrs)))
+                (Ob_tilde_over_to_mor_to (dd_from_C (Tilde_dd_pr1 (pr1 itrs)))
+                                         (Tilde_from_C_to_Ob_tilde_over (Tilde_dd_pr1 (pr1 itrs)))))
+             (ll Z - ll (dd (Tilde_dd_pr1 (pr1 itrs)))) 
+             (natminuslehn (ll Z) (ll (dd (Tilde_dd_pr1 (pr1 itrs)))))
+             (! isabove_to_isover inn) ) .
+  
+  
+  
+
+Definition Mor_lB0_from_C_to_Mor { CC : lCsystem } ( X Y : CC )
+           ( f : @Mor_from_B ( lB0_from_C CC ) X Y ) : X --> Y . 
+Proof .
+  intros . 
+
+
+
+
 
   
 
